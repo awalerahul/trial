7261d63eda0c62a84918c62ecb30655c7736f769
AMOL <AMOL@amol>
2022-07-24 10:55:43 +0530

    second changes

--- src/com/aspire/sample/FIrst_git.java
+++ src/com/aspire/sample/FIrst_git.java
@@ -6,5 +6,6 @@ public class FIrst_git
 	public static void main(String[] args) 
 	{
 		System.out.println("first push on git");
+		System.out.println("new changes");
 	}
 }
---
+++ src/com/aspire/sample/Second_git.java
@@ -0,0 +1,9 @@
+package com.aspire.sample;
+
+public class Second_git 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("second class");
+	}
+}
